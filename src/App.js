9d37ff6c94220248ef10191fa58f2b96e05e88ff
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2026-02-20 14:00:51 +0100

    fix : stylisation page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ function Header(){
   return <h1>Hello World</h1>;
 }
 function App() {
-  return <Header/> 
+  return ( 
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Header /> 
+    </div> 
+  ); 
 }
 
 export default App;
