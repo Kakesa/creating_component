6bdd7be781af341a62abfd853b6fa36254d11d8f
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2026-02-22 07:01:24 +0100

    feat: creation components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 function Header(){
   return <h1>Hello World</h1>;
 }
+
+function Exemple(){return <h1>Exemple</h1>;}
 function App() {
   return ( 
     <div className="App"> 
       This is the starting code for "Your first component" ungraded lab 
       <Header /> 
+      <Exemple/>
     </div> 
   ); 
 }
